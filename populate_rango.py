7536b726e0252097c38b4eec6063c9f728be4e16
Charlotte Leeming <2379824L@student.gla.ac.uk>
2020-02-11 12:41:12 +0000

    chapter 5 exercises done (started chpter 6 by accident)

--- populate_rango.py
+++ populate_rango.py
@@ -29,17 +29,18 @@ def populate():
 		{'title':'Flask',
 			'url':'http://flask.pocoo.org'} 
 			]
-	cats = {'Python': {'pages': python_pages},
-			'Django': {'pages': django_pages},
-			'Other Frameworks': {'pages': other_pages} }
+	cats = {'Python': {'pages': python_pages, 'likes': 128, 'views': 64},
+			'Django': {'pages': django_pages, 'likes': 64, 'views': 32},
+			'Other Frameworks': {'pages': other_pages, 'likes': 32, 'views': 16} }
 	for cat, cat_data in cats.items():
-		c = add_cat(cat)
+		c = add_cat(cat,cat_data['views'],cat_data['likes'])
 		for p in cat_data['pages']:
 			add_page(c, p['title'], p['url'])
 
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
 			print(f'- {c}: {p}')
+	
 
 def add_page(cat, title, url, views=0):
 	p = Page.objects.get_or_create(category=cat, title=title)[0]
@@ -48,8 +49,10 @@ def add_page(cat, title, url, views=0):
 	p.save()
 	return p
 
-def add_cat(name):
-	c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0,likes=0):
+	c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
+	c.views = views
+	c.likes = likes
 	c.save()
 	return c
 
